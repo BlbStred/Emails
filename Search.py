a85d7c5893e05e3a63fcff03cbe7c87c0cf64a46
BlbStred <a.dan.brand@gmail.com>
2026-08-15 19:49:56 -1000

    Added plain body to messages

--- Search.py
+++ Search.py
@@ -63,7 +63,54 @@ def get_gmail_service():
 gmailService = get_gmail_service() # To access gmail
 
 
+import base64
 
+def get_message_body(payload):
+    """
+    Extracts text/plain or text/html body from a full Gmail API message object.
+    Returns a dict: {'plain': str, 'html': str}
+    """
+    
+    bodies = {'plain': '', 'html': ''}
+
+    def parse_parts(parts):
+        
+        for part in parts:
+            mime_type = part.get('mimeType')
+            body_data = part.get('body', {}).get('data')
+
+            if body_data:
+                # Gmail API uses URL-safe base64 encoding (- and _ instead of + and /)
+                decoded_bytes = base64.urlsafe_b64decode(body_data)
+                decoded_text = decoded_bytes.decode('utf-8', errors='ignore')
+
+                if mime_type == 'text/plain' and not bodies['plain']:
+                    bodies['plain'] = decoded_text
+                elif mime_type == 'text/html' and not bodies['html']:
+                    bodies['html'] = decoded_text
+
+            # Recursively handle nested multi-part structures
+            if 'parts' in part:
+                parse_parts(part['parts'])
+
+    # 1. Simple Single-Part Message
+    
+    if 'data' in payload.get('body', {}):
+        body_data = payload['body']['data']
+        decoded_bytes = base64.urlsafe_b64decode(body_data)
+        decoded_text = decoded_bytes.decode('utf-8', errors='ignore')
+        
+        mime_type = payload.get('mimeType', 'text/plain')
+        if mime_type == 'text/html':
+            bodies['html'] = decoded_text
+        else:
+            bodies['plain'] = decoded_text
+
+    # 2. Multi-Part Message (e.g., multipart/alternative, multipart/mixed)
+    elif 'parts' in payload:
+        parse_parts(payload['parts'])
+
+    return bodies
 
 
 def getEmailList(category, ignoreIdList):
@@ -94,7 +141,7 @@ def getEmailList(category, ignoreIdList):
     for msg in messages:
         # Check whether precessed previously
         msgId = msg['id']
-
+        
                 
         # msg provides id only, fetch full message details
         message = gmailService.users().messages().get(userId='me', id=msgId).execute()
@@ -102,6 +149,8 @@ def getEmailList(category, ignoreIdList):
         # Extract headers 
         payload = message.get('payload', {})
         headers = payload.get('headers', [])
+
+        body = get_message_body(payload)['plain']
         
         # headers is a list of dictionaries {'name: ..., 'value': ...}
         # To get the subject, for example, find the first dictionary {'name: 'Subject', 'value': ...}
@@ -110,7 +159,7 @@ def getEmailList(category, ignoreIdList):
         date    = next((h['value'] for h in headers if h['name'] == 'Date'),    'Unknown Date')
         date    = re.split(r'[+-]', date)[0]     # Get rid of the universal time at the end
 
-        emailList.append(EmailMessage(msgId, sender, subject, date, category))
+        emailList.append(EmailMessage(msgId, sender, subject, date, category, body))
 
     return emailList
 
@@ -137,19 +186,21 @@ def getEmailIdList(category):
 #######################################
 
 class EmailMessage:
-    def __init__(self, id, sender, subject, date, category):
+    def __init__(self, id, sender, subject, date, category, body):
         self.id      = str(id)
         self.sender  = str(sender)
         self.subject = str(subject)
         self.date    = str(date)
-        self.category= str(category)                
+        self.category= str(category)
+        self.body    = str(body)                        
 
     def __str__(self):
         result  =                self.id
         result += " from: "    + self.sender
         result += " subject: " + self.subject
         result += " date0: "   + self.date
-        result += " in: "      + self.category                
+        result += " in: "      + self.category
+        result += " body: "    + self.body                        
         return result
 
 
@@ -341,4 +392,4 @@ if __name__ == '__main__':
     
     for e in emails:
         if "Watch" not in e.subject:
-            print(e.subject)
+            print("\n\n", e, flush=True)
