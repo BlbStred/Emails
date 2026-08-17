78d331c5a5886dfc881ca3885e1d82b83bc65cc4
BlbStred <a.dan.brand@gmail.com>
2026-08-17 09:52:28 -1000

    import my from common directory

--- Search.py
+++ Search.py
@@ -1,21 +1,19 @@
-
-
-
-
-
-
-
 import sys
 import EmailMessage
 import GmailService
+from pathlib import Path
 
+commonDir = "C:\\Users\\Dan\\Documents\\Computing\\common"
+if commonDir not in sys.path:
+    sys.path.insert(0, str(Path(commonDir).resolve()))
 
-# Reconfigure stdout to handle replace unprintable character with something like '?'
-# instead of crashing
-sys.stdout.reconfigure(errors='replace')
+import my
+
 
 if __name__ == '__main__':
    
+    my.init()
+    
     gmailService = GmailService.GmailService()
     
     ignore = []
